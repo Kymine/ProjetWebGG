ea8e0a5f8a506dcb7ebe0106b76ae6b971ef8e67
SebastienDegand <sebastien.degand@etu.unice.fr>
2017-06-12 17:34:24 +0100

    seb: ChannelModel modif

--- src/app/channels/channel-list/channel-list.component.ts
+++ src/app/channels/channel-list/channel-list.component.ts
@@ -18,6 +18,28 @@ export class ChannelListComponent implements OnInit {
     this.currentChannelRoute = new ChannelModel(1 , "general");
   }
 
+  deleteChannel(id : number){
+    let list : ChannelModel[];
+    let j : number;
+    j=0;
+    for( let i = 0; i < this.channelList.length; i++){
+      if(this.channelList[i].id != id){
+        list[j]=this.channelList[i];
+        j++;
+      }
+    }
+    this.channelList=list;
+  }
+
+  addChannel(id: number, name: string){
+    for ( let i =0; i < this.channelList.length; i++){
+      if(this.channelList[i].id == id){
+        return;
+      }
+    }
+    this.channelList[this.channelList.length]=new ChannelModel(id,name);
+  }
+
 
 
   ngOnInit() {
