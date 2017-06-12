af3dcd26d18abf1cf72e9c0f1e88d778d3b7ebd3
SebastienDegand <sebastien.degand@etu.unice.fr>
2017-06-12 16:50:29 +0100

    seb: ChannelModel modif

--- src/app/channels/channel-list/channel-list.component.ts
+++ src/app/channels/channel-list/channel-list.component.ts
@@ -1,3 +1,26 @@
-/**
- * Created by Pierre on 12/06/2017.
- */
+import { Component, OnInit } from "@angular/core";
+
+import { ChannelModel } from "../../../shared/models/ChannelModel";
+
+@Component({
+  selector: "app-channel-list",
+  templateUrl: "./channel-list.component.html",
+  styleUrls: ["./channel-list.component.css"]
+})
+
+export class ChannelListComponent implements OnInit {
+
+  public channelList: ChannelModel[];
+
+  public currentChannelRoute: ChannelModel;
+
+  constructor() {
+    this.currentChannelRoute = new ChannelModel(1,"general");
+  }
+
+
+  ngOnInit() {
+
+  }
+
+}
