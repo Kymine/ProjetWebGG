9803b8f056bf5c3fb1229eba3eb1261c01183f45
SebastienDegand <sebastien.degand@etu.unice.fr>
2017-06-13 17:52:16 +0100

    seb: creation des services pour les messages privée

---
+++ src/shared/services/privateChannel/privateChannel.service.spec.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by sebde on 13/06/2017.
+ */
---
+++ src/shared/services/privateChannel/privateChannel.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@angular/core";
+import {Headers, Http, RequestOptions, Response} from "@angular/http";
+import {URLSERVER} from "../../constants/urls";
+import {ReplaySubject} from "rxjs/ReplaySubject";
+@Injectable()
+export class PrivateChannelService {
+
+  private url: string;
+  public userList$: ReplaySubject<string[]>;
+
+  constructor(private http: Http) {
+    this.url = "http://projet-3a.7ight.com/api/users";
+    this.userList$ = new ReplaySubject(1);
+  }
+
+
+  public getUsers() {
+    this.http.get(this.url)
+      .subscribe((response) => this.extractAndUpdatePrivateChannelList(response));
+  }
+
+
+  extractAndUpdatePrivateChannelList(response: Response) {
+    const privatechannelList = response.json() || [];
+    this.userList$.next(privatechannelList);
+  }
+}
