10a5fa9993a62299b41c4cbf052cc58501f1d93d
SebastienDegand <sebastien.degand@etu.unice.fr>
2017-06-13 14:03:46 +0100

    seb: modif channel service

--- src/app/channels/channel-list/channel-list.component.ts
+++ src/app/channels/channel-list/channel-list.component.ts
@@ -16,32 +16,7 @@ export class ChannelListComponent implements OnInit {
   public currentChannelRoute: ChannelModel;
 
   constructor(private channelService: ChannelService) {
-    // this.currentChannelRoute = new ChannelModel(1, "general");
   }
-
-  /*deleteChannel(id: number) {
-   let list: ChannelModel[];
-   list = null;
-   let j: number;
-   j = 0;
-   for (let i = 0; i < this.channelList.length; i++) {
-   if (this.channelList[i].id !== id) {
-   list[j] = this.channelList[i];
-   j++;
-   }
-   }
-   this.channelList = list;
-   }*/
-
-  /*addChannel(id: number, name: string) {
-   for ( let i = 0; i < this.channelList.length; i++) {
-   if (this.channelList[i].id === id) {
-   return;
-   }
-   }
-   this.channelList[this.channelList.length] = new ChannelModel(id, name);
-   }*/
-
   ngOnInit() {
     this.channelService.getChannels(0);
     this.channelService.channelList$.subscribe((channels) => this.channelList = channels);
